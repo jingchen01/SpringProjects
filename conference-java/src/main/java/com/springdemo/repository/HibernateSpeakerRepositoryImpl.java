4e3396f16350c1b8329b4a2526bb22b87203bb28
jingchen01 <landscape.chen@gmail.com>
2020-09-16 13:45:25 -0700

    Add dev profile

--- conference-java/src/main/java/com/springdemo/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-java/src/main/java/com/springdemo/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,12 +2,14 @@ package com.springdemo.repository;
 
 import com.springdemo.model.Speaker;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository("speakerRepository")
+@Profile("dev")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
     @Value("#{ T(java.lang.Math).random() * 100 }")
--- conference-java/src/main/java/com/springdemo/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/springdemo/service/SpeakerServiceImpl.java
@@ -3,11 +3,13 @@ package com.springdemo.service;
 import com.springdemo.model.Speaker;
 import com.springdemo.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
